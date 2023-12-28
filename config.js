f290e7ac45b4b2b4265e43023fd0be80c88c14f8
beymight <155069409+beymight@users.noreply.github.com>
2023-12-28 17:06:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['08132165823', 'me', true],
+  ['08132165823', 'beymight', true], 
   [''] 
 ] //Number of owners
 
